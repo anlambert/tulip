f95bb26fff12c2add65948973f0a8f3a205f065d
Antoine Lambert <antoine.lambert33@gmail.com>
2020-01-16 19:35:13 +0100

    talipot-python/PythonIDE: Fix plugin file loading in code editor
    The regression was introduced in commit 5c1523b.

--- library/talipot-python/src/PythonIDE.cpp
+++ library/talipot-python/src/PythonIDE.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2020  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -727,7 +727,7 @@ static bool checkAndGetPluginInfoFromSrcCode(const QString &pluginCode, QString 
       pos = rx.indexIn(pluginCode, pos + rx.matchedLength());
     }
 
-    rx.setPattern("^.*register.*Plugin.*\\(.*,.*\"([^,]+)\",.*$");
+    rx.setPattern("^.*registerPlugin.*\\(.*['\"]([^,]+)['\"],.*$");
 
     if (rx.indexIn(pluginCode) != -1) {
       pluginName = rx.cap(1);
