bb6e532f61b926e0e5a5aa0d9408a9125318b9eb
Antoine Lambert <antoine.lambert33@gmail.com>
2020-02-21 22:28:31 +0100

    utils: Add git pre-commit to update years range in license headers

--- utils/git-hooks/setup-hooks.sh
+++ utils/git-hooks/setup-hooks.sh
@@ -4,8 +4,13 @@ set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 
+cp $ROOT/utils/git-hooks/pre-commit $ROOT/.git/hooks/pre-commit
+chmod +x $ROOT/.git/hooks/pre-commit
+
 cp $ROOT/utils/git-hooks/pre-commit-clang-format $ROOT/.git/hooks/pre-commit-clang-format
 chmod +x $ROOT/.git/hooks/pre-commit-clang-format
 
-cp $ROOT/utils/git-hooks/pre-commit $ROOT/.git/hooks/pre-commit
-chmod +x $ROOT/.git/hooks/pre-commit
+cp $ROOT/utils/git-hooks/pre-commit-license-years $ROOT/.git/hooks/pre-commit-license-years
+chmod +x $ROOT/.git/hooks/pre-commit-license-years
+
+
