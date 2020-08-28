dfbf6b4d4829ecf3c325ee06d7ad1fa6f15b6554
Antoine Lambert <antoine.lambert33@gmail.com>
2020-08-28 21:59:30 +0200

    appveyor/msvc: Use vcpkg to get and compile almost all dependencies [ci-windows-only]

--- appveyor_msvc.bat
+++ appveyor_msvc.bat
@@ -19,70 +19,19 @@ set PATH=C:/Python35-x64;C:/Python35-x64/Scripts;%PATH%
 pip install clcache
 set CLCACHE_MSBUILD_CONF=/p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\Python35-x64\Scripts
 
-rem create a directory to store Talipot dependencies
+rem create a directory to store some Talipot dependencies
 cd C:/ && md talipot_dependencies
 cd C:/talipot_dependencies && md bin
 cd C:/talipot_dependencies && md include
 cd C:/talipot_dependencies && md lib
 
-rem get, compile and install zlib
-cd C:/talipot_dependencies
-curl -LO https://zlib.net/zlib1211.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x zlib1211.zip
-cd zlib-1.2.11
-md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
+cd C:\Tools\vcpkg
+git pull
+call bootstrap-vcpkg.bat
+cd %APPVEYOR_BUILD_FOLDER%
 
-rem get, compile and install qhull
-cd C:/talipot_dependencies
-curl -LO http://www.qhull.org/download/qhull-2019.1.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x qhull-2019.1.zip
-cd qhull-2019.1
-md build_qhull && cd build_qhull
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-rem get, compile and install yajl
-cd C:/talipot_dependencies
-git clone git://github.com/lloyd/yajl
-if %errorlevel% neq 0 exit /b %errorlevel%
-cd yajl
-md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-rem get, compile and install zstd
-curl -LO https://github.com/facebook/zstd/releases/download/v1.4.5/zstd-1.4.5.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x zstd-1.4.5.tar.gz -so | 7z x -aoa -si -ttar
-cd zstd-1.4.5/build/cmake
-md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-rem Fix zstd cmake bug not installing dll (fixed upstream but not released yet)
-copy lib\Release\zstd.dll C:\talipot_dependencies\bin\
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-rem get, compile and install cppunit
-cd C:/talipot_dependencies
-git clone git://anongit.freedesktop.org/git/libreoffice/cppunit/
-if %errorlevel% neq 0 exit /b %errorlevel%
-cd cppunit/src
-msbuild CppUnitLibraries2010.sln /m /clp:ErrorsOnly /p:Configuration=Release /p:Platform=%MSVC_PLATFORM% /p:PlatformToolset=%MSVC_PLATFORM_TOOLSET% %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
-xcopy C:\talipot_dependencies\cppunit\include C:\talipot_dependencies\include /S /Y
-xcopy C:\talipot_dependencies\cppunit\lib C:\talipot_dependencies\lib /S /Y
-move C:\talipot_dependencies\lib\cppunit_dll.dll C:\talipot_dependencies\bin\
+rem install talipot core build dependencies
+vcpkg install --triplet x64-windows zlib zstd qhull yajl cppunit
 
 if "%TALIPOT_BUILD_CORE_ONLY%" == "0" (
   goto install_complete_talipot_build_dependencies
@@ -93,36 +42,16 @@ if "%TALIPOT_BUILD_CORE_ONLY%" == "0" (
 
 :install_complete_talipot_build_dependencies
 
-rem get, compile and install freetype
-cd C:/talipot_dependencies
-curl -LO http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x freetype-2.8.tar.gz -so | 7z x -aoa -si -ttar
-cd freetype-2.8
-md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-rem get, compile and install glew
-cd C:/talipot_dependencies
-curl -LO https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x glew-2.1.0.zip
-cd glew-2.1.0/build/cmake
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" .
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
+rem install talipot complete build dependencies
+vcpkg install --triplet x64-windows freetype glew
 
 rem get, compile and install quazip
 cd C:/talipot_dependencies
-curl -LO https://github.com/stachenov/quazip/archive/v0.8.1.zip
-7z x v0.8.1.zip
-cd quazip-0.8.1
+curl -LO https://github.com/stachenov/quazip/archive/v0.9.1.zip
+7z x v0.9.1.zip
+cd quazip-0.9.1
 md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DZLIB_INCLUDE_DIRS="%QT5_DIR%/include/QtZlib" ..
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot_dependencies" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DCMAKE_INCLUDE_PATH="C:/Tools/vcpkg/installed/x64-windows/include" -DCMAKE_LIBRARY_PATH="C:/Tools/vcpkg/installed/x64-windows/lib" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -133,7 +62,7 @@ goto talipot_build
 rem we are good to go, let's compile and install Talipot now
 cd %APPVEYOR_BUILD_FOLDER%
 md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot" -DCMAKE_INCLUDE_PATH="C:/talipot_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/talipot_dependencies/lib;C:/talipot_dependencies/bin" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DPYTHON_EXECUTABLE="%PYTHON_EXECUTABLE%" -DTALIPOT_BUILD_CORE_ONLY=%TALIPOT_BUILD_CORE_ONLY% -DTALIPOT_BUILD_TESTS=ON ..
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/talipot" -DCMAKE_INCLUDE_PATH="C:/Tools/vcpkg/installed/x64-windows/include;C:/talipot_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/Tools/vcpkg/installed/x64-windows/bin;C:/Tools/vcpkg/installed/x64-windows/lib;C:/talipot_dependencies/lib;C:/talipot_dependencies/bin" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DPYTHON_EXECUTABLE="%PYTHON_EXECUTABLE%" -DTALIPOT_BUILD_CORE_ONLY=%TALIPOT_BUILD_CORE_ONLY% -DTALIPOT_BUILD_TESTS=ON ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /verbosity:minimal /m /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
