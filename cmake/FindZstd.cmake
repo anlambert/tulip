5044bc51c8a46a113dd4dfd3234785bd1e47ff7f
Antoine Lambert <antoine.lambert33@gmail.com>
2020-08-29 00:29:05 +0200

    cmake/FindZstd: Fix warning

--- cmake/FindZstd.cmake
+++ cmake/FindZstd.cmake
@@ -1,24 +1,24 @@
 # Find the zstd compression library and includes
 
 FIND_PATH(
-  zstd_INCLUDE_DIRS
+  Zstd_INCLUDE_DIRS
   NAMES zstd.h
   HINTS ${zstd_ROOT_DIR}/include)
 
 FIND_LIBRARY(
-  zstd_LIBRARIES
+  Zstd_LIBRARIES
   NAMES zstd
   HINTS ${zstd_ROOT_DIR}/lib)
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(zstd DEFAULT_MSG zstd_LIBRARIES
-                                  zstd_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(zstd DEFAULT_MSG Zstd_LIBRARIES
+                                  Zstd_INCLUDE_DIRS)
 
-MARK_AS_ADVANCED(zstd_LIBRARIES zstd_INCLUDE_DIRS)
+MARK_AS_ADVANCED(Zstd_LIBRARIES Zstd_INCLUDE_DIRS)
 
 IF(zstd_FOUND AND NOT (TARGET zstd::zstd))
   ADD_LIBRARY(zstd::zstd UNKNOWN IMPORTED)
   SET_TARGET_PROPERTIES(
-    zstd::zstd PROPERTIES IMPORTED_LOCATION ${zstd_LIBRARIES}
-                          INTERFACE_INCLUDE_DIRECTORIES ${zstd_INCLUDE_DIRS})
+    zstd::zstd PROPERTIES IMPORTED_LOCATION ${Zstd_LIBRARIES}
+                          INTERFACE_INCLUDE_DIRECTORIES ${Zstd_INCLUDE_DIRS})
 ENDIF()
