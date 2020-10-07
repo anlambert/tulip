a1380022d9e0aca7de4fed1e59a486cdde71934d
Antoine Lambert <antoine.lambert@inria.fr>
2020-10-08 01:07:07 +0200

    appveyor/msys2: Revert 1f8684cec5c11da4a193e314c7db4cc9692e563a.
    https://github.com/msys2/MINGW-packages/issues/7084#issuecomment-705210651

--- appveyor_msys2.bat
+++ appveyor_msys2.bat
@@ -7,15 +7,8 @@ rem    MSYSTEM:  MINGW64 or MINGW32
 rem Update PATH for MSYS2 binaries
 PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 
-rem Override MSYS2 package server temporarily (https://github.com/msys2/MINGW-packages/issues/7084)
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/mingw/x86_64/' > /etc/pacman.d/mirrorlist.mingw64"
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/mingw/i686/' > /etc/pacman.d/mirrorlist.mingw32"
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/msys/$arch/' > /etc/pacman.d/mirrorlist.msys"
-
 rem Update MSYS2 keyring (see https://www.msys2.org/news/#2020-06-29-new-packagers)
-rem bash -lc "pacman --noconfirm -U http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
-
-bash -lc "pacman --noconfirm -U https://mirror.yandex.ru/mirrors/msys2/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+bash -lc "pacman --noconfirm -U http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
 
 rem Upgrade pacman first
 rem (see https://www.msys2.org/news/#2020-05-31-update-fails-with-could-not-open-file)
@@ -23,11 +16,6 @@ bash -lc "pacman --noconfirm -Sydd pacman"
 
 rem Upgrade MSYS2 platform
 bash -lc "pacman --noconfirm -Syu"
-
-rem Override MSYS2 package server temporarily (https://github.com/msys2/MINGW-packages/issues/7084)
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/mingw/x86_64/' > /etc/pacman.d/mirrorlist.mingw64"
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/mingw/i686/' > /etc/pacman.d/mirrorlist.mingw32"
-bash -lc "echo 'Server = https://mirror.yandex.ru/mirrors/msys2/msys/$arch/' > /etc/pacman.d/mirrorlist.msys"
 
 set BUILD_WHEELS=%1
 
