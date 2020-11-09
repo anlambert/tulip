anlambert/tulip

commit b67d466b8d9caae9bfc852f7a76bd46faa0648ae

talipot-python/PythonInterpreter: Remove deprecated Python API calls

library/talipot-python/src/PythonInterpreter.cpp
@@ -268,8 +268,9 @@ PythonInterpreter::PythonInterpreter()
 #else
     PySys_SetArgv(argc, const_cast<char **>(argv));
 #endif
-
+#if PY_VERSION_HEX < 0x03090000
     PyEval_InitThreads();
+#endif
     mainThreadState = PyEval_SaveThread();
   }
 
@@ -390,7 +391,6 @@ PythonInterpreter::~PythonInterpreter() {
     consoleOuputString = "";
     runString(
         "sys.stdout = sys.__stdout__; sys.stderr = sys.__stderr__; sys.stdin = sys.__stdin__\n");
-    PyEval_ReleaseLock();
     PyEval_RestoreThread(mainThreadState);
 
     holdGIL();

commit 37508c9bdec61c337583c47d22e3ff08b0d5b379

talipot-python/PythonInterpreter: Fix linux deadlock with Python < 3.9
Regression introduced in b67d466.

library/talipot-python/src/PythonInterpreter.cpp
@@ -391,6 +391,9 @@ PythonInterpreter::~PythonInterpreter() {
     consoleOuputString = "";
     runString(
         "sys.stdout = sys.__stdout__; sys.stderr = sys.__stderr__; sys.stdin = sys.__stdin__\n");
+#if PY_VERSION_HEX < 0x03090000
+    PyEval_ReleaseLock();
+#endif
     PyEval_RestoreThread(mainThreadState);
 
     holdGIL();