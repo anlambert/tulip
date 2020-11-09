64c6783804804c4da28c58c0f508b04924973232
Antoine Lambert <antoine.lambert@inria.fr>
2020-11-09 21:01:46 +0100

    ci: Migrate MacOS builds from TravisCI to AppVeyor
    travis-ci.org will officially close down on on December 31st, 2020
    https://mailchi.mp/3d439eeb1098/travis-ciorg-is-moving-to-travis-cicom
    
    Users have to migrate on travis-ci.com before that date but unfortunately
    that migration comes with new pricing which basically kills support for
    public OSS repositories. Current accounts will be migrated to a free trial
    plan with 1000 minutes linux build time that will not be renewed each month
    but rather need to be negotiated.
    https://blog.travis-ci.com/2020-11-02-travis-ci-new-billing
    
    So it is time to move away from TravisCI in favor of CI service providers
    that still offer unlimited build jobs for public git repositories.
    
    AppVeyor has a great MacOS support and their VM seem fasters so let's
    move MacPorts build jobs there.
    
    Linux and MacOS Homebrew builds will be migrated later to Github Actions.

---
+++ bundlers/mac/talipot_python_wheels_macos_build.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Script to build and upload Talipot Python wheels for MacOS on AppVeyor
+
+# abort script on first error
+set -e
+
+# get last uploaded dev wheel version
+JSON=$(curl -s 'https://test.pypi.org/pypi/talipot/json')
+LAST_VERSION=$(echo $JSON | python -c "
+import sys, json
+print(json.load(sys.stdin)['info']['version'])" 2>/dev/null)
+echo last wheel dev version = $LAST_VERSION
+
+# check if dev wheel version needs to be incremented
+VERSION_INCREMENT=$(echo $JSON | python -c "
+import sys, json
+releases = json.load(sys.stdin)['releases']['$LAST_VERSION']
+print(any(['macosx' in r['filename'] for r in releases]))")
+DEV_VERSION=$(echo $LAST_VERSION | cut -f4 -d '.' | sed 's/dev//')
+
+if [ "$VERSION_INCREMENT" == "True" ]
+then
+  let DEV_VERSION+=1
+fi
+echo current wheel dev version = $DEV_VERSION
+
+# install MacPorts
+curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
+source ./macports-ci install
+
+CLANG_VERSION=10
+
+# install build tools and dependencies
+sudo port -N install \
+  cmake \
+  clang-${CLANG_VERSION} \
+  ccache \
+  qhull \
+  yajl \
+  zlib \
+  zstd
+
+# install Python versions from which to build wheels against
+curl -LO https://www.python.org/ftp/python/2.7.17/python-2.7.17-macosx10.9.pkg
+sudo installer -pkg python-2.7.17-macosx10.9.pkg -target /
+sudo /Library/Frameworks/Python.framework/Versions/2.7/bin/pip install wheel
+curl -LO https://www.python.org/ftp/python/3.7.6/python-3.7.6-macosx10.9.pkg
+sudo installer -pkg python-3.7.6-macosx10.9.pkg -target /
+sudo /Library/Frameworks/Python.framework/Versions/3.7/bin/pip3 install wheel
+curl -LO https://www.python.org/ftp/python/3.8.1/python-3.8.1-macosx10.9.pkg
+sudo installer -pkg python-3.8.1-macosx10.9.pkg -target /
+sudo /Library/Frameworks/Python.framework/Versions/3.8/bin/pip3 install wheel
+curl -LO https://www.python.org/ftp/python/3.9.0/python-3.9.0-macosx10.9.pkg
+sudo installer -pkg python-3.9.0-macosx10.9.pkg -target /
+sudo /Library/Frameworks/Python.framework/Versions/3.9/bin/pip3 install wheel
+sudo /Library/Frameworks/Python.framework/Versions/3.9/bin/pip3 install twine
+
+# configure and build wheels for all supported Python versions
+cd $APPVEYOR_BUILD_FOLDER
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PWD/install \
+  -DCMAKE_C_COMPILER=/opt/local/bin/clang-mp-${CLANG_VERSION} \
+  -DCMAKE_CXX_COMPILER=/opt/local/bin/clang++-mp-${CLANG_VERSION} \
+  -DTALIPOT_ACTIVATE_PYTHON_WHEEL_TARGET=ON \
+  -DTALIPOT_PYTHON_TEST_WHEEL_SUFFIX=dev$DEV_VERSION \
+  -DPYTHON_EXECUTABLE=/Library/Frameworks/Python.framework/Versions/2.7/bin/python \
+  -DTALIPOT_USE_CCACHE=ON \
+  -DTALIPOT_BUILD_CORE_ONLY=ON \
+  -DTALIPOT_BUILD_DOC=OFF
+
+make -j4 test-wheel
+
+for py3Version in 3.7 3.8 3.9
+do
+  cmake .. \
+    -DPYTHON_EXECUTABLE=/Library/Frameworks/Python.framework/Versions/$py3Version/bin/python3
+  make -j4 test-wheel
+done
+
+# upload wheels to test PyPI
+if [ "$APPVEYOR_REPO_BRANCH" == "master" ]
+then
+  make test-wheel-upload
+fi
+
+# test uploaded wheels in clean environement
+sudo port -N -f uninstall \
+  cmake \
+  clang-${CLANG_VERSION} \
+  ccache \
+  qhull \
+  yajl \
+  zlib \
+  zstd
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+sudo pip install --index-url https://test.pypi.org/simple/ talipot
+python -c "from talipot import tlp; print(tlp.getLayoutAlgorithmPluginsList())"
+
+sudo port -N install python39
+sudo port -N install py39-pip
+sudo pip-3.9 install --index-url https://test.pypi.org/simple/ talipot
+python3.9 -c "from talipot import tlp; print(tlp.getLayoutAlgorithmPluginsList())"
+
+sudo pip3.7 install --index-url https://test.pypi.org/simple/ talipot
+python3.7 -c "from talipot import tlp; print(tlp.getLayoutAlgorithmPluginsList())"
