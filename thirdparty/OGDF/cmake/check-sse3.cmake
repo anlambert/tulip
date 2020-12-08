99d89c8cb6f71a59bf9deea5eb8eaaabfc196a9e
Antoine Lambert <antoine.lambert@inria.fr>
2020-12-08 21:02:49 +0100

    thirdparty/OGDF/cmake: Fix SSE3 detection when using GCC

--- thirdparty/OGDF/cmake/check-sse3.cmake
+++ thirdparty/OGDF/cmake/check-sse3.cmake
@@ -1,5 +1,8 @@
 include(CheckCXXSourceCompiles)
 
+if(NOT MSVC)
+  set(CMAKE_REQUIRED_FLAGS -msse3)
+endif()
 check_cxx_source_compiles("
 #include <pmmintrin.h>
 int main() {
@@ -9,6 +12,9 @@ int main() {
 }" has_sse3_pmmintrin)
 
 if (NOT has_sse3_pmmintrin)
+  if(NOT MSVC)
+    set(CMAKE_REQUIRED_FLAGS -sse3)
+  endif()
   check_cxx_source_compiles("
 #include <intrin.h>
 int main() {
