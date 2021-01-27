565cb23252af3c43d28c685307a1649511bb7520
Antoine Lambert <antoine.lambert@inria.fr>
2021-01-27 18:02:10 +0100

    talipot-core/TreeTest: Simplify isFreeTree implementation
    As a free tree is an undirected graph with no root node and no cycles,
    testing on numbers of graph elements and connectivity is sufficient,
    no need to add an extra DFS pass looking for cycles.
    
    Ported from https://github.com/Tulip-Dev/tulip/commit/aed83992b9d6f0ad9d69bed7a17be4302a62e8fd

--- library/talipot-core/include/talipot/TreeTest.h
+++ library/talipot-core/include/talipot/TreeTest.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -91,9 +91,6 @@ public:
    *graph->getRoot()->delNode()).
    **/
   static void cleanComputedTree(Graph *graph, Graph *tree);
-
-private:
-  static bool isFreeTree(const Graph *graph, node curRoot);
 };
 }
 #endif // TALIPOT_TREE_TEST_H
--- library/talipot-core/src/TreeTest.cpp
+++ library/talipot-core/src/TreeTest.cpp
@@ -102,9 +102,9 @@ bool TreeTest::isTree(const tlp::Graph *graph) {
 // Determines if a graph is a topological tree.  This means that
 // if the graph was undirected, there would be no cycle
 bool TreeTest::isFreeTree(const tlp::Graph *graph) {
-  node firstNode = graph->getOneNode();
-  return (firstNode.isValid() && isFreeTree(graph, firstNode)) ? ConnectedTest::isConnected(graph)
-                                                               : false;
+  auto nbNodes = graph->numberOfNodes();
+  auto nbEdges = graph->numberOfEdges();
+  return nbNodes && (nbEdges == nbNodes - 1) && ConnectedTest::isConnected(graph);
 }
 //====================================================================
 // simple structure to implement
@@ -122,57 +122,6 @@ struct dfsFreeTreeStruct {
     }
   }
 };
-
-// Determines if the given graph is topologically a tree
-bool TreeTest::isFreeTree(const Graph *graph, node curRoot) {
-  // do a dfs traversal from curRoot;
-  MutableContainer<bool> visited;
-  visited.setAll(false);
-  stack<dfsFreeTreeStruct> dfsLevels;
-  dfsFreeTreeStruct curParams(curRoot, curRoot, graph->getInOutNodes(curRoot));
-  dfsLevels.push(curParams);
-
-  while (!dfsLevels.empty()) {
-    curParams = dfsLevels.top();
-    curRoot = curParams.curRoot;
-    node cameFrom = curParams.cameFrom;
-    Iterator<node> *neighbours = curParams.neighbours;
-    // set neighbours member to nullptr
-    // to avoid twice deletion on exit
-    curParams.neighbours = nullptr;
-
-    if (!neighbours->hasNext()) {
-      dfsLevels.pop();
-    } else {
-      visited.set(curRoot.id, true);
-
-      // loop on remaining neighbours
-      while (neighbours->hasNext()) {
-        node curNode = neighbours->next();
-
-        // check self loop
-        if (curNode == curRoot) {
-          return false;
-        }
-
-        if (curNode != cameFrom) {
-          if (visited.get(curNode.id)) {
-            return false;
-          }
-
-          // go deeper in the dfs exploration
-          curParams.curRoot = curNode;
-          curParams.cameFrom = curRoot;
-          curParams.neighbours = graph->getInOutNodes(curNode);
-          dfsLevels.push(curParams);
-          break;
-        }
-      }
-    }
-  }
-
-  return true;
-}
 //====================================================================
 // simple structure to implement
 // the further makeRootedTree dfs loop
