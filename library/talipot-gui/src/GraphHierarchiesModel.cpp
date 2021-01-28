d90be9eefb4551a144a364cf0b9922418c12900e
Antoine Lambert <antoine.lambert@inria.fr>
2021-01-28 21:47:08 +0100

    talipot-gui/GraphHierarchiesModel: Fix subgraph deletion handling
    Subgraph deletion handling in graph hierarchies qt model must be performed
    when receiving the TLP_AFTER_DEL_DESCENDANTGRAPH event.
    
    Previously it was done when receiving the TLP_BEFORE_DEL_DESCENDANTGRAPH
    event, resulting in tree views not properly updated as qt paint events
    were sent before the subgraph was actually deleted in its parent graph.

--- library/talipot-gui/src/GraphHierarchiesModel.cpp
+++ library/talipot-gui/src/GraphHierarchiesModel.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -873,7 +873,7 @@ void GraphHierarchiesModel::treatEvent(const Event &e) {
         // when the treatEvents method is called
         _graphsChanged.insert(parentGraph);
 
-      } else if (ge->getType() == GraphEvent::TLP_BEFORE_DEL_DESCENDANTGRAPH) {
+      } else if (ge->getType() == GraphEvent::TLP_AFTER_DEL_DESCENDANTGRAPH) {
         // that event must only be treated on a root graph
         if (ge->getGraph() != ge->getGraph()->getRoot()) {
           return;
