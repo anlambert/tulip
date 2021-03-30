anlambert/tulip

commit 411ff922df5d9eb8cbc89638938aa19fe118a216

talipot-core: Improve code readability with C++17
Use C++17 Structured Bindings to improve readability of map iterations
or pair manipulations.

library/talipot-core/src/GraphTools.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -106,14 +106,14 @@ void makeProperDag(Graph *graph, list<node> &addedNodes,
   unsigned int nbEdges = edges.size();
   for (unsigned int i = 0; i < nbEdges; ++i) {
     edge e = edges[i];
-    auto eEnds = graph->ends(e);
-    unsigned int fLevel = dLevel.getNodeValue(eEnds.first);
-    unsigned int sLevel = dLevel.getNodeValue(eEnds.second);
+    const auto &[src, tgt] = graph->ends(e);
+    unsigned int fLevel = dLevel.getNodeValue(src);
+    unsigned int sLevel = dLevel.getNodeValue(tgt);
     int delta = sLevel - fLevel;
 
     if (delta > 1) {
       node n1 = graph->addNode();
-      replacedEdges[e] = graph->addEdge(eEnds.first, n1);
+      replacedEdges[e] = graph->addEdge(src, n1);
       addedNodes.push_back(n1);
       dLevel.addNodeValue(n1, fLevel + 1);
 
@@ -130,12 +130,12 @@ void makeProperDag(Graph *graph, list<node> &addedNodes,
         n1 = n2;
       }
 
-      graph->addEdge(n1, eEnds.second);
+      graph->addEdge(n1, tgt);
     }
   }
 
-  for (const auto &it : replacedEdges) {
-    graph->delEdge(it.first);
+  for (const auto &[e, ee] : replacedEdges) {
+    graph->delEdge(e);
   }
 
   assert(AcyclicTest::isAcyclic(graph));
@@ -494,9 +494,9 @@ void selectMinimumSpanningTree(Graph *graph, BooleanProperty *selection,
     unsigned int srcClass = 0, tgtClass = 0;
 
     for (; iE < nbEdges; ++iE) {
-      auto curEnds = graph->ends(cur = sortedEdges[iE]);
+      const auto &[src, tgt] = graph->ends(cur = sortedEdges[iE]);
 
-      if ((srcClass = classes[curEnds.first]) != (tgtClass = classes[curEnds.second])) {
+      if ((srcClass = classes[src]) != (tgtClass = classes[tgt])) {
         break;
       }
     }
@@ -679,9 +679,9 @@ void buildNodesUniformQuantification(const Graph *graph, const NumericProperty *
   double cK = double(nbNodes) / double(k);
   int k2 = 0;
 
-  for (const auto &it : histogram) {
-    sum += it.second;
-    nodeMapping[it.first] = k2;
+  for (const auto &[val, count] : histogram) {
+    sum += count;
+    nodeMapping[val] = k2;
 
     if (sum > cK * (k2 + 1)) {
       k2 = ceil(sum / cK) - 1;
@@ -709,9 +709,9 @@ void buildEdgesUniformQuantification(const Graph *graph, const NumericProperty *
   double cK = double(graph->numberOfEdges()) / double(k);
   int k2 = 0;
 
-  for (const auto &it : histogram) {
-    sum += it.second;
-    edgeMapping[it.first] = k2;
+  for (const auto &[val, count] : histogram) {
+    sum += count;
+    edgeMapping[val] = k2;
 
     if (sum > cK * (k2 + 1)) {
       k2 = ceil(sum / cK) - 1;
@@ -723,14 +723,14 @@ unsigned makeSelectionGraph(const Graph *graph, BooleanProperty *selection, bool
   Observable::holdObservers();
   unsigned added = 0;
   for (auto e : selection->getEdgesEqualTo(true, graph)) {
-    auto ends = graph->ends(e);
+    const auto &[src, tgt] = graph->ends(e);
 
-    if (!selection->getNodeValue(ends.first)) {
+    if (!selection->getNodeValue(src)) {
 #ifndef NDEBUG
-      tlp::debug() << "[Make selection a graph] node #" << ends.first.id << " source of edge #"
-                   << e.id << " automatically added to selection.";
+      tlp::debug() << "[Make selection a graph] node #" << src.id << " source of edge #" << e.id
+                   << " automatically added to selection.";
 #endif
-      selection->setNodeValue(ends.first, true);
+      selection->setNodeValue(src, true);
       added++;
 
       if (test) {
@@ -739,12 +739,12 @@ unsigned makeSelectionGraph(const Graph *graph, BooleanProperty *selection, bool
       }
     }
 
-    if (!selection->getNodeValue(ends.second)) {
+    if (!selection->getNodeValue(tgt)) {
 #ifndef NDEBUG
-      tlp::debug() << "[Make selection a graph] node #" << ends.second.id << " target of edge #"
-                   << e.id << " automatically added to selection.";
+      tlp::debug() << "[Make selection a graph] node #" << tgt << " target of edge #" << e.id
+                   << " automatically added to selection.";
 #endif
-      selection->setNodeValue(ends.second, true);
+      selection->setNodeValue(tgt, true);
       added++;
 
       if (test) {

library/talipot-core/include/talipot/ConsoleUtils.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -500,11 +500,11 @@ inline std::ostream &fillToEndOfLine(std::ostream &s) {
   if (processInForeground() && ((&s == &std::cout && isatty(fileno(stdout))) ||
                                 (&s == &std::cerr && isatty(fileno(stderr))))) {
 #endif
-    std::pair<int, int> cursorPos = getConsoleCursorPosition();
-    std::pair<int, int> consoleSize = getConsoleSize();
+    auto [row, col] = getConsoleCursorPosition();
+    auto [rows, cols] = getConsoleSize();
 
-    if ((cursorPos.second + consoleSize.second) != 0) {
-      for (int i = cursorPos.second; i <= consoleSize.second; ++i) {
+    if ((col + cols) != 0) {
+      for (int i = col; i <= cols; ++i) {
         s << " ";
       }
     } else {

library/talipot-core/include/talipot/DataSet.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -156,8 +156,8 @@ class DataTypeSerializerContainer {
 
 public:
   ~DataTypeSerializerContainer() {
-    for (const auto &it : tnTodts) {
-      delete it.second;
+    for (const auto &[type, serializer] : tnTodts) {
+      delete serializer;
     }
   }
 

library/talipot-core/include/talipot/GraphStorage.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -285,9 +285,9 @@ public:
    */
   node opposite(const edge e, const node n) const {
     assert(isElement(e));
-    const std::pair<node, node> &eEnds = edgeEnds[e.id];
-    assert((eEnds.first == n) || (eEnds.second == n));
-    return (eEnds.first == n) ? eEnds.second : eEnds.first;
+    const auto &[src, tgt] = edgeEnds[e.id];
+    assert((src == n) || (tgt == n));
+    return (src == n) ? tgt : src;
   }
 
   //=======================================================

library/talipot-core/include/talipot/PluginsManager.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -233,13 +233,13 @@ private:
   template <typename PluginType>
   std::list<std::string> availablePluginsImpl() {
     std::list<std::string> keys;
-    for (const auto &it : _plugins) {
-      PluginType *plugin = dynamic_cast<PluginType *>(it.second.info);
+    for (const auto &[pluginName, pluginDescription] : _plugins) {
+      PluginType *plugin = dynamic_cast<PluginType *>(pluginDescription.info);
 
       if (plugin != nullptr &&
           // deprecated names are not listed
-          it.first == it.second.info->name()) {
-        keys.push_back(it.first);
+          pluginName == pluginDescription.info->name()) {
+        keys.push_back(pluginName);
       }
     }
 

library/talipot-core/include/talipot/SortIterator.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -67,10 +67,10 @@ struct LessThanEdgeExtremitiesMetric {
   LessThanEdgeExtremitiesMetric(const Graph *sg, const tlp::NumericProperty *metric)
       : metric(metric), sg(sg) {}
   bool operator()(const edge &e1, const edge &e2) const {
-    std::pair<node, node> ends = sg->ends(e1);
-    Vec2d v1(metric->getNodeDoubleValue(ends.first), metric->getNodeDoubleValue(ends.second));
-    ends = sg->ends(e2);
-    Vec2d v2(metric->getNodeDoubleValue(ends.first), metric->getNodeDoubleValue(ends.second));
+    const auto &[src1, tgt1] = sg->ends(e1);
+    Vec2d v1 = {metric->getNodeDoubleValue(src1), metric->getNodeDoubleValue(tgt1)};
+    const auto &[src2, tgt2] = sg->ends(e2);
+    Vec2d v2 = {metric->getNodeDoubleValue(src2), metric->getNodeDoubleValue(tgt2)};
     return v1 < v2;
   }
 

library/talipot-core/include/talipot/cxx/MinMaxProperty.cxx
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -245,11 +245,10 @@ void tlp::MinMaxProperty<nodeType, edgeType, propType>::updateNodeValue(
 
     if (newValue != oldV) {
       // loop on subgraph min/max
-      for (const auto &it : minMaxNode) {
+      for (const auto &[graphId, minMax] : minMaxNode) {
         // if min/max is ok for the current subgraph
         // check if min or max has to be updated
-        typename nodeType::RealType minV = it.second.first;
-        typename nodeType::RealType maxV = it.second.second;
+        const auto &[minV, maxV] = minMax;
 
         // check if min or max has to be updated
         if ((newValue < minV) || (newValue > maxV) || (oldV == minV) || (oldV == maxV)) {
@@ -270,11 +269,10 @@ void tlp::MinMaxProperty<nodeType, edgeType, propType>::updateEdgeValue(
 
     if (newValue != oldV) {
       // loop on subgraph min/max
-      for (const auto &it : minMaxEdge) {
+      for (const auto &[graphId, minMax] : minMaxEdge) {
         // if min/max is ok for the current subgraph
         // check if min or max has to be updated
-        typename edgeType::RealType minV = it.second.first;
-        typename edgeType::RealType maxV = it.second.second;
+        const auto &[minV, maxV] = minMax;
 
         // check if min or max has to be updated
         if ((newValue < minV) || (newValue > maxV) || (oldV == minV) || (oldV == maxV)) {
@@ -291,11 +289,10 @@ void tlp::MinMaxProperty<nodeType, edgeType, propType>::updateAllNodesValues(
     typename nodeType::RealType newValue) {
 
   // loop on subgraph min/max
-  MINMAX_PAIR(nodeType) minmax(newValue, newValue);
+  MINMAX_PAIR(nodeType) minmax = {newValue, newValue};
 
-  for (const auto &it : minMaxNode) {
-    unsigned int gid = it.first;
-    minMaxNode[gid] = minmax;
+  for (const auto &[graphId, minMax] : minMaxNode) {
+    minMaxNode[graphId] = minmax;
   }
 }
 
@@ -303,11 +300,10 @@ template <typename nodeType, typename edgeType, typename propType>
 void tlp::MinMaxProperty<nodeType, edgeType, propType>::updateAllEdgesValues(
     typename edgeType::RealType newValue) {
   // loop on subgraph min/max
-  MINMAX_PAIR(edgeType) minmax(newValue, newValue);
+  MINMAX_PAIR(edgeType) minmax = {newValue, newValue};
 
-  for (const auto &it : minMaxEdge) {
-    unsigned int gid = it.first;
-    minMaxEdge[gid] = minmax;
+  for (const auto &[graphId, minMax] : minMaxEdge) {
+    minMaxEdge[graphId] = minmax;
   }
 }
 

library/talipot-core/include/talipot/cxx/MutableContainer.cxx
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019-2020  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -99,8 +99,8 @@ void tlp::MutableContainer<TYPE>::setAll(typename StoredType<TYPE>::ReturnedCons
 
     if (StoredType<TYPE>::isPointer) {
       // delete stored values
-      for (const auto &it : *hData) {
-        StoredType<TYPE>::destroy(it.second);
+      for (const auto &[id, type] : *hData) {
+        StoredType<TYPE>::destroy(type);
       }
     }
 
@@ -519,9 +519,9 @@ void tlp::MutableContainer<TYPE>::hashtovect() {
   elementInserted = 0;
   state = VECT;
 
-  for (const auto &it : *hData) {
-    if (it.second != defaultValue) {
-      vectset(it.first, it.second);
+  for (const auto &[id, type] : *hData) {
+    if (type != defaultValue) {
+      vectset(id, type);
     }
   }
 

commit 84c43b9426b8b1890dd6ee146bff3f2f93393097

talipot-core/GraphTools: Fix heap-use-after-free
The regression was introduced in 411ff92.

library/talipot-core/src/GraphTools.cpp
@@ -106,7 +106,7 @@ void makeProperDag(Graph *graph, list<node> &addedNodes,
   unsigned int nbEdges = edges.size();
   for (unsigned int i = 0; i < nbEdges; ++i) {
     edge e = edges[i];
-    const auto &[src, tgt] = graph->ends(e);
+    auto [src, tgt] = graph->ends(e);
     unsigned int fLevel = dLevel.getNodeValue(src);
     unsigned int sLevel = dLevel.getNodeValue(tgt);
     int delta = sLevel - fLevel;