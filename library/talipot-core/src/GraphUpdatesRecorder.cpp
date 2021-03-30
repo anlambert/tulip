anlambert/tulip

commit 64b743d68d8e4c7f918038b0a6de13ef13228ae4

talipot-core/GraphUpdatesRecorder: Fix bug when deleting added loops
Recorded node adjacency after a graph push was incorrect when adding a new loop,
leading to invalid computed node degrees after a graph pop.

Add a new test case in PushPopTest to showcase the issue.

library/talipot-core/src/GraphUpdatesRecorder.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2020  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -210,6 +210,10 @@ void GraphUpdatesRecorder::recordEdgeContainer(
       // it must be at the last adj position
       auto &adj = containers[n];
       auto size = adj.size() - 1;
+      if (g->opposite(e, n) == n) {
+        // handle loop special case
+        size -= 1;
+      }
       assert(e == adj[size]);
       adj.resize(size);
     }
@@ -226,18 +230,13 @@ void GraphUpdatesRecorder::recordEdgeContainer(
     // so we look (in reverse order because they must be at the end)
     // for the elts of adj that are in the last edges added and remove them
     unsigned int adjAdded = 0;
-    unsigned int lastAdded = gEdges.size();
-    for (unsigned int i = adj.size() - 1; i > 0; --i) {
-      edge e = adj[i];
-      while (nbAdded) {
-        --nbAdded;
-        if (e == gEdges[--lastAdded]) {
-          ++adjAdded;
-          break;
-        }
-      }
-      if (nbAdded == 0)
+    for (auto e : reversed(adj)) {
+      if (std::find(gEdges.begin() + (gEdges.size() - nbAdded - 1), gEdges.end(), e) !=
+          gEdges.end()) {
+        ++adjAdded;
+      } else {
         break;
+      }
     }
     assert(adjAdded);
     adj.resize(adj.size() - adjAdded);

tests/library/talipot/PushPopTest.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2020  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -1330,4 +1330,42 @@ void PushPopTest::testDeletePushPopFalse() {
 
   // same thing for the two deleted properties
   CPPUNIT_ASSERT_EQUAL(size_t(4), delObserver.deletedProperties.size());
+}
+
+void PushPopTest::testAddDelLoopsOneByOne() {
+  node n = graph->addNode();
+  node n2 = graph->addNode();
+  graph->addEdge(n, n2);
+  graph->push();
+  graph->addEdge(n2, n2);
+  graph->addEdge(n2, n2);
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 5u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 3u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 2u);
+  graph->pop();
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 1u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 1u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 0u);
+  graph->unpop();
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 5u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 3u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 2u);
+}
+
+void PushPopTest::testAddDelLoopsBatch() {
+  node n = graph->addNode();
+  node n2 = graph->addNode();
+  graph->push();
+  graph->addEdges({{n, n2}, {n, n2}, {n2, n2}, {n2, n2}});
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 6u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 4u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 2u);
+  graph->pop();
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 0u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 0u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 0u);
+  graph->unpop();
+  CPPUNIT_ASSERT_EQUAL(graph->deg(n2), 6u);
+  CPPUNIT_ASSERT_EQUAL(graph->indeg(n2), 4u);
+  CPPUNIT_ASSERT_EQUAL(graph->outdeg(n2), 2u);
 }

tests/library/talipot/PushPopTest.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2020  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -38,6 +38,8 @@ class PushPopTest : public CppUnit::TestFixture {
   CPPUNIT_TEST(testObserveDelProps);
   CPPUNIT_TEST(testAddSubgraphProp);
   CPPUNIT_TEST(testMetaNode);
+  CPPUNIT_TEST(testAddDelLoopsOneByOne);
+  CPPUNIT_TEST(testAddDelLoopsBatch);
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -65,6 +67,8 @@ public:
   void testObserveDelProps();
   void testAddSubgraphProp();
   void testMetaNode();
+  void testAddDelLoopsOneByOne();
+  void testAddDelLoopsBatch();
 };
 
 #endif // PUSH_POP_TEST_H

commit 5a273d768657a4d1f0c66925ce977f285f6404b6

talipot-core/GraphUpdatesRecorder: Fix heap-buffer-overflow
The regression was introduced in 64b743d.

library/talipot-core/src/GraphUpdatesRecorder.cpp
@@ -228,8 +228,7 @@ void GraphUpdatesRecorder::recordEdgeContainer(
     // for the elts of adj that are in the last edges added and remove them
     unsigned int adjAdded = 0;
     for (auto e : reversed(adj)) {
-      if (std::find(gEdges.begin() + (gEdges.size() - nbAdded - 1), gEdges.end(), e) !=
-          gEdges.end()) {
+      if (std::find(gEdges.end() - nbAdded, gEdges.end(), e) != gEdges.end()) {
         ++adjAdded;
       } else {
         break;