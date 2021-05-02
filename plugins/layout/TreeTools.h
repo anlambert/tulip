6f9beda8a1b2615270fed3bbd84d07b199594ce1
Antoine Lambert <antoine.lambert@inria.fr>
2021-05-02 14:35:30 +0200

    plugins/layout/TreeTools: Robustify isLeaf implementation
    This prevent possible segfaults in the Improved Walker layout algorithm.

--- plugins/layout/TreeTools.h
+++ plugins/layout/TreeTools.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2019  The Talipot developers
+ * Copyright (C) 2019-2021  The Talipot developers
  *
  * Talipot is a fork of Tulip, created by David Auber
  * and the Tulip development Team from LaBRI, University of Bordeaux
@@ -24,7 +24,7 @@ class Graph;
 
 //====================================================================
 inline bool isLeaf(const tlp::Graph *tree, tlp::node n) {
-  return tree->outdeg(n) == 0;
+  return tree->isElement(n) && tree->outdeg(n) == 0;
 }
 
 //====================================================================
